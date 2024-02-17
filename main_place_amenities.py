baf48b542b7c9e65306b31390e1876a116884468
Ermiyas <ermiyasabiye1999@gmail.com>
2024-02-17 19:37:12 +0300

    fixed indentation in list of states

--- main_place_amenities.py
+++ main_place_amenities.py
@@ -5,14 +5,20 @@ from models import *
 
 # creation of a State
 state = State(name="California")
+state2 = State(name="Alabama")
+state2.save()
 state.save()
 
 # creation of a City
 city = City(state_id=state.id, name="San Francisco")
+city1 = City(state_id=state2.id, name="Tupelo")
+city1.save()
 city.save()
 
 # creation of a User
 user = User(email="john@snow.com", password="johnpwd")
+user1 = User(email="ermiyas@abiye.com", password="ermipwd")
+user1.save()
 user.save()
 
 # creation of 2 Places
@@ -20,6 +26,8 @@ place_1 = Place(user_id=user.id, city_id=city.id, name="House 1")
 place_1.save()
 place_2 = Place(user_id=user.id, city_id=city.id, name="House 2")
 place_2.save()
+place_3 = Place(user_id=user1.id, city_id=city1.id, name="House 3")
+place_3.save()
 
 # creation of 3 various Amenity
 amenity_1 = Amenity(name="Wifi")
@@ -37,6 +45,10 @@ place_1.amenities.append(amenity_2)
 place_2.amenities.append(amenity_1)
 place_2.amenities.append(amenity_2)
 place_2.amenities.append(amenity_3)
+
+place_3.amenities.append(amenity_1)
+place_3.amenities.append(amenity_2)
+place_3.amenities.append(amenity_3)
 
 storage.save()
 
