488a22baf23325aa8e8238eb17bcd097a6a8ed6c
Ermiyas <ermiyasabiye1999@gmail.com>
2024-02-17 19:45:45 +0300

    fixed indentation in list of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,16 +6,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown(exc):
-    """remove the current sqlalchemy session"""
-    storage.close()
-
-
 @app.route("/states_list", strict_slashes=False)
 def cities_by_states():
     """fetches the storage engine and renders html template"""
     return render_template("7-states_list.html", states=storage.all(State))
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """remove the current sqlalchemy session"""
+    storage.close()
 
 
 if __name__ == "__main__":
